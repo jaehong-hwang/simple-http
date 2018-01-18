67afa43341294c2282d3954acaad7bca0dc41d7d
Jaehong-Hwang <krusnocide@gmail.com>
2018-01-18 18:18:37 +0900

    Get error -> fatal, 바디 출력되도록 수정

--- test/server_test.go
+++ test/server_test.go
@@ -7,6 +7,7 @@ import (
 	simpleHttp "bitbucket.org/jaehong-blog/simple-http"
 	"time"
 	"strconv"
+	"io/ioutil"
 )
 
 var port string
@@ -24,15 +25,22 @@ func Test_Listen(t *testing.T) {
 
 	iPort, _ := strconv.Atoi(port)
 	srv := s.Listen(iPort)
-	
+
 	defer srv.Close()
 
 	res, err := http.Get("http://localhost:" + port)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	defer res.Body.Close()
+
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log("response body: ", string(data))
 
 	if res.StatusCode != http.StatusOK {
 		t.Error(res)
