7067185677ed6cfd91ae057248e29b5225649ebe
Jaehong-Hwang <krusnocide@gmail.com>
2018-08-26 12:32:33 +0900

    db struct to golang database/sql, query test 추가

--- database/database.go
+++ database/database.go
@@ -4,48 +4,19 @@ import (
 	"database/sql"
 )
 
-// Pool struct
-type Pool struct {
-	Env          Env
-	MaxOpenConns int
-	MaxIdleConns int
-	SQLDB        *sql.DB
-}
-
-// NewPool func
-func NewPool(env Env, maxOpenConns, maxIdleConns int) (Pool, error) {
-	db := Pool{
-		Env:          env,
-		MaxOpenConns: maxOpenConns,
-		MaxIdleConns: maxIdleConns,
-	}
-
-	if err := db.Open(); err != nil {
-		return Pool{}, err
-	}
-
-	return db, nil
-}
-
-// Open DB Pool
-func (p *Pool) Open() error {
-	var err error
-	p.SQLDB, err = sql.Open(p.Env.Drive, p.Env.GetDataSourceName())
+// Connect to db func
+func Connect(env Env, maxOpenConns, maxIdleConns int) (*sql.DB, error) {
+	db, err := sql.Open(env.Drive, env.GetDataSourceName())
 
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	if err = p.SQLDB.Ping(); err != nil {
-		return err
+	if err = db.Ping(); err != nil {
+		return nil, err
 	}
 
-	p.SQLDB.SetMaxOpenConns(p.MaxOpenConns)
-	p.SQLDB.SetMaxIdleConns(p.MaxIdleConns)
-	return nil
-}
-
-// Close pool
-func (p *Pool) Close() error {
-	return p.SQLDB.Close()
+	db.SetMaxOpenConns(maxOpenConns)
+	db.SetMaxIdleConns(maxIdleConns)
+	return db, nil
 }
--- test/db_test.go
+++ test/db_test.go
@@ -1,23 +1,72 @@
 package test
 
 import (
+	"database/sql"
 	"testing"
 
 	_ "github.com/go-sql-driver/mysql"
 	sdb "github.com/jaehong-hwang/simple-http/database"
 )
 
-var db sdb.Pool
+func connect(env sdb.Env) (*sql.DB, error) {
+	db, err := sdb.Connect(env, 10, 10)
+	return db, err
+}
 
 func TestConnection(t *testing.T) {
-	var err error
 	env := GetDatabaseEnv()
-
-	if db, err = sdb.NewPool(env, 10, 10); err != nil {
+	db, err := connect(env)
+	if err != nil {
 		t.Fatal(env.GetDataSourceName(), err.Error())
 	}
 
 	defer db.Close()
 
 	t.Log("db connected")
+}
+
+type testStruct struct {
+	f1 string
+	f2 int
+}
+
+func TestQuery(t *testing.T) {
+	env := GetDatabaseEnv()
+	db, err := connect(env)
+	if err != nil {
+		t.Fatal(env.GetDataSourceName(), err.Error())
+	}
+
+	var f1 string
+	var f2 int
+
+	var datas []testStruct
+	datas = append(datas, testStruct{f1: "a", f2: 1})
+	datas = append(datas, testStruct{f1: "b", f2: 2})
+	datas = append(datas, testStruct{f1: "c", f2: 3})
+	_, err = db.Query("CREATE TABLE golang_query_test (f1 VARCHAR(20), f2 INT)")
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	t.Log("Insert Log ===")
+	for _, data := range datas {
+		t.Logf("f1 : %s, f2 : %d", data.f1, data.f2)
+		_, err = db.Query("INSERT INTO golang_query_test (f1, f2) VALUES (?, ?)", data.f1, data.f2)
+		if err != nil {
+			t.Fatal(err.Error())
+		}
+	}
+
+	rows, err := db.Query("SELECT * FROM golang_query_test")
+	t.Log("Select result ===")
+	for rows.Next() {
+		rows.Scan(&f1, &f2)
+		t.Logf("f1 : %s, f2 : %d", f1, f2)
+	}
+
+	_, err = db.Query("DROP TABLE golang_query_test")
+	if err != nil {
+		t.Fatal(err.Error())
+	}
 }
